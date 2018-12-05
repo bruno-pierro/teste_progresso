bf5a8f2cf4bda845322501e2761345bcd8f05900
Bruno Pierro <bc.pierro1@gmail.com>
2018-12-05 11:05:08 -0200

    adição coord

--- coord.php
+++ coord.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="css/professor.css">
@@ -33,15 +33,15 @@
 							<tr class="monta1 ">
 								<th>
 									<!-- <input list="materias" style="width:100%" class="questao1" id="materia_selecionada"> -->
-									<select id="materias" style="width:100%" class="questao1" onchange="select_materia_prova(this)">
+									<select id="materias" style="width:100%" class="questao1 ddlMaterias" onchange="select_materia_prova(this)">
 										<?php 
 										include("materia_prova.php")
 									
 										?>
 									</select>
 								</th>
-								<th><input type="number" min="1" max="10" name="quantidade1" class="questao1" style="width:100%"></th>
-								<th><input type="number" min="1" max="3" name="dificuldade" class="questao1" style="width:100%"></th>
+								<th><input type="number" min="1" max="10" name="quantidade1" class="questao1 ddlQtd" style="width:100%"></th>
+								<th><input type="number" min="1" max="3" name="dificuldade" class="questao1 ddlDificuldade" style="width:100%"></th>
 								<th><button type="button" class="btn btn-secondary questao1 bt1" onclick="checa()">Inserir</button></th>
 							</tr>
 						</tbody>
