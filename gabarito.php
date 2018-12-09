90bac0713c81dc24bce30821a132b24d00aa30cd
Bruno Pierro <bc.pierro1@gmail.com>
2018-12-09 19:44:35 -0200

    função gabarito

---
+++ gabarito.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="gabarito-block">
+        <!-- <p>No: {num_questao}</p> -->
+        <div style="border-bottom: 2px solid black"></div>
+        <h1>GABARITO</h1>
+        <p style="display: inline-flex"><span class="numero_questao"></span> 
+            <span class="questao">
+            <div>
+	<p>1: B</p>
+</div><div>
+	<p>2: A</p>
+</div><div>
+	<p>3: B</p>
+</div><div>
+	<p>4: A</p>
+</div><div>
+	<p>5: B</p>
+</div><div>
+	<p>6: A</p>
+</div><div>
+	<p>7: A</p>
+</div><div>
+	<p>8: A</p>
+</div><div>
+	<p>9: A</p>
+</div><div>
+	<p>10: D</p>
+</div><div>
+	<p>11: D</p>
+</div><div>
+	<p>12: C</p>
+</div><div>
+	<p>13: {alt_correta}</p>
+</div>
+            </span>
+        </p>
+
+            <!-- [ALTERNATIVAS] -->
+        </div>
+
+</body>
+</html>
